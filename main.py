58f38e0cbc8b0b610a141845446c9ba2af45aeda
Tom <abaotom14@gmail.com>
2022-07-13 15:39:23 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+gridarr = [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]
+]
+
+square = {
+    0: None,
+    1: None,
+    2: None,
+    3: None,
+    4: None
+}
+
+grid = {
+    0: square,
+    1: square,
+    2: square,
+    3: square,
+    4: square
+}
+
+def main():
+    print('Hello, World!')
+
+
+if __name__ == '__main__':
+    main()
